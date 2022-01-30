2fd5c96c78e662683dd434c80052039839609d2a
Asher Pembroke <apembroke@gmail.com>
2022-01-30 13:45:46 -0600

    Deployed 4e8ea8f with MkDocs version: 1.2.3

---
+++ javascript/utterances.js
@@ -0,0 +1,7 @@
+<script src="https://utteranc.es/client.js"
+        repo="https://github.com/asherp/bitcoin_physics"
+        issue-term="pathname"
+        theme="preferred-color-scheme"
+        crossorigin="anonymous"
+        async>
+</script>
